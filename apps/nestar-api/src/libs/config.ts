6e2b1f097aac745a30a7140155fe4d5cc6b7893b
Abrahamjondev <ibrohimjon0425@gmail.com>
2026-04-17 17:01:15 +0500

    feat: develop getProperties  GraphQl API

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -3,6 +3,16 @@ import { ObjectId } from 'bson';
 export const availableAgentSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews', 'memberRank'];
 export const availableMemberSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews'];
 
+export const availableOptions = ['propertyBarter', 'propertyRent'];
+export const availablePropertySorts = [
+	'createdAt',
+	'updatedAt',
+	'propertyPrice',
+	'propertyViews',
+	'propertyLikes',
+	'propertyRank',
+];
+
 /**IMAGE CONFIGURATION (config.js)**/
 import { v4 as uuidv4 } from 'uuid';
 import * as path from 'path';
@@ -15,4 +25,13 @@ export const getSerialForImage = (filename: string) => {
 
 export const shapeIntoMongoObjectid = (target: any) => {
 	return typeof target === 'string' ? new ObjectId(target) : target;
+};
+
+export const lookupMember = {
+	$lookup: {
+		from: 'members',
+		localField: 'memberId',
+		foreignField: '_id',
+		as: 'memberData',
+	},
 };
