c1a0c949fb7d583b11bf29c2ff921b17f8b90076
Abrahamjondev <ibrohimjon0425@gmail.com>
2026-04-18 08:48:43 +0500

    feat: develop removePropertyByAdmin  GraphQl API

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -95,4 +95,13 @@ export class PropertyResolver {
 		input._id = shapeIntoMongoObjectid(input._id);
 		return await this.propertyService.updatePropertyByAdmin(input);
 	}
+
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Mutation((returns) => Property)
+	public async removePropertyByAdmin(@Args('propertyId') input: string): Promise<Property> {
+		console.log('Mutation: removePropertyByAdmin');
+		const propertyId = shapeIntoMongoObjectid(input);
+		return await this.propertyService.removePropertyByAdmin(propertyId);
+	}
 }
