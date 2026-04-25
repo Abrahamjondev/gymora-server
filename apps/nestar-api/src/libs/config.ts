533cc176e4b4317d1455955cb035717e3f839778
Abrahamjondev <ibrohimjon0425@gmail.com>
2026-04-25 12:12:44 +0500

    feat: develop getfavorite GraphQL API

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -122,3 +122,11 @@ export const lookupFollowerData = {
 		as: 'followerData',
 	},
 };
+export const lookupFavorite = {
+	$lookup: {
+		from: 'members',
+		localField: 'favoriteProperty.memberId',
+		foreignField: '_id',
+		as: 'favoriteProperty.memberData',
+	},
+};
